fe2c86e7847e1d2e26f9fd560e73cc96966afab0
Beforerr <zzj956959688@gmail.com>
2024-10-17 15:26:46 -0700

    refactor: for transition matrix code

--- src/io.jl
+++ src/io.jl
@@ -1,35 +1,40 @@
 using DataFramesMeta
+using CurrentSheetTestParticle
 
 #%%
 subset_leave(df) = @subset(df, :t1 .!= :tmax)
 subset_trap(df) = @subset(df, :t1 .== :tmax)
 
-function get_results(; dir="simulations")
-    dfs = collect_results(datadir(dir))
-
-    for r in eachrow(dfs)
-        get_result(r)
-    end
-
+function get_result_dfs(; dir="simulations")
     @chain begin
-        vcat(dfs[!, :results]...)
-        @transform!(:α0 = acosd.(:w0), :α1 = acosd.(:w1))
-        @transform!(:Δα = :α1 .- :α0)
-    end
+        collect_results(datadir(dir))
+        @rtransform(
+            :B = RD_B_field(; θ=:θ, β=:β, sign=:sign)
+        )
+        @transform!(
+            :θ = round.(rad2deg.(:θ)), 
+            :β = round.(rad2deg.(:β)),
+            :tmax = last.(:tspan)
+        )
+        select!(Not(:tspan))
+    end 
 end
 
-function get_result(r)
-    @unpack θ, β, sign, v, alg_sym, tspan = r
-    params = [:θ, :β, :sign, :v, :alg_sym]
-    df = copy(r[:results])
+function get_result(; dir="simulations")
+    dfs = get_result_dfs(; dir)
+    results = map(get_result, eachrow(dfs))
+    vcat(results...)
+end
+
+function get_result(r::DataFrameRow)
+    params = [:θ, :β, :sign, :v, :alg_sym, :tmax, :B]
     @chain df begin
         insertcols!(
-            :B => RD_B_field(; θ, β, sign),
-            :tmax => tspan[2],
             Pair.(params, Array(r[params]))...; makeunique=true
         )
         @rtransform!(:w0 = :wϕ0[1], :ϕ0 = :wϕ0[2], :w1 = cos_pitch_angle(:u1, :B))
-        transform!([:θ, :β] .=> ByRow(rad2deg), renamecols=false)
+        @transform!(:α0 = acosd.(:w0), :α1 = acosd.(:w1))
+        @transform!(:Δα = :α1 .- :α0)
         select!(Not(:wϕ0))
     end
 end
