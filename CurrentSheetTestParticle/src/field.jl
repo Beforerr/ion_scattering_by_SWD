30862f3f7ac43b9b96df44f93ae985e79017894b
Beforerr <zzj956959688@gmail.com>
2024-10-18 17:30:19 -0700

    update field.jl to use degrees for θ and β in RD_B_field function

--- CurrentSheetTestParticle/src/field.jl
+++ CurrentSheetTestParticle/src/field.jl
@@ -11,12 +11,12 @@ Rotating magnetic field.
 # Notes
 φ = β * tanh(z) is the polar angle
 """
-function RD_B_field(r; B=1, θ=π/2, β=π/2, sign=1)
+function RD_B_field(r; B=1, θ=45, β=90, sign=1)
     z = r[3]
     φ = β * tanh(z)
-    Bz = B * cos(θ)
-    Bx = B * sin(θ) * sin(φ)
-    By = sign * B * sin(θ) * cos(φ)
+    Bz = B * cosd(θ)
+    Bx = B * sind(θ) * sind(φ)
+    By = sign * B * sind(θ) * cosd(φ)
     return SVector(Bx, By, Bz)
 end
 
