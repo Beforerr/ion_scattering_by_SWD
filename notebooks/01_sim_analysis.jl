caaef47c611deb62aff4067e5f0fbfaeda4e2c75
Beforerr <zzj956959688@gmail.com>
2024-10-21 10:21:54 -0700

    feat: add test param examples

--- notebooks/01_sim_analysis.jl
+++ notebooks/01_sim_analysis.jl
@@ -12,18 +12,34 @@ df = get_result(; dir);
 θs = vals(df, :θ)
 βs = vals(df, :β)
 
-d = (;θ = 85, β = 47.5, layout=:v)
-sdf = @subset(df, :θ .== d.θ, :β .== d.β)
-v = histogram(; bins=64)
+scale = scales(Color=(;
+    lowclip=:transparent,
+    colorrange=(0.01, 10),
+))
 
-plt0 = data(sdf) * mapping(xyw...; layout=v_map) * v
-fg = draw(plt0; axis=w_axis)
-fg
+d = (; θ=85, β=47.5, layout=:v)
+sdf = @subset(df, :θ .== d.θ, :β .== d.β)
+let v = histogram(; bins=64, normalization=:pdf)
+    plt0 = data(sdf) * mapping(xyw...; layout=v_map) * v
+    draw(plt0, scale; axis=w_axis)
+    easy_save(savename("pa", d))
+end
+
+# ----
+# Test
+# ----
+let dir = "test"
+    df = get_result(; dir);
+    ldf, rdf = split_results(df);
+    pa_pair_plot(ldf, rdf)
+    easy_save("tm/example")
+end
+
 
 plt = data(subset_leave(sdf)) * mapping(xyw...; layout=v_map) * v
 draw(plt; axis=w_axis)
-Label(fg.figure[0,1:end], "θ = $(d.θ)°, β = $(d.β)°")
-easy_save(savename("pa", d), dir=figure_dir)
+Label(fg.figure[0, 1:end], "θ = $(d.θ)°, β = $(d.β)°")
+
 
 plt2 = data(subset_trap(sdf)) * mapping(xyw...; layout=v_map) * v
 draw(plt2; axis=w_axis)
@@ -36,4 +52,4 @@ t = :t1 => "Δt"
 plt0 = layer * mapping(w0, ϕ0, color=t)
 fg = draw(plt0)
 
-Label(fg.figure[0,1:end], "θ = $(d.θ)°, β = $(d.β)°")
+Label(fg.figure[0, 1:end], "θ = $(d.θ)°, β = $(d.β)°")
