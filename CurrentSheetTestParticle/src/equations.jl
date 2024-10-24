6c3849bafe782817369711d2a42644976056457c
Beforerr <zzj956959688@gmail.com>
2024-10-23 18:43:39 -0700

    Refactor equations module and add reciprocal of Lorentz factor function

--- CurrentSheetTestParticle/src/equations.jl
+++ CurrentSheetTestParticle/src/equations.jl
@@ -4,6 +4,15 @@ using TestParticle: c
 const c2 = c^2
 
 """
+Reciprocal of Lorentz factor for relativistic particle.
+"""
+function gamma_reciprocal(vx, vy, vz; v0=1)
+    u2 = (vx^2 + vy^2 + vz^2) * v0^2
+    u2 ≥ c2 && throw(DomainError(u2, FTLError))
+    return √(1 - u2 / c2)
+end
+
+"""
 Normalized ODE equations for charged particle moving in static magnetic field with in-place form.
 
 Optimized version of `trace_normalized!` for the case of static magnetic field.
@@ -15,9 +24,9 @@ function trace_normalized_B!(du, u, p, t)
     Bx, By, Bz = B(u, t)
 
     du[1], du[2], du[3] = vx, vy, vz
-    du[4] = vy*Bz - vz*By
-    du[5] = vz*Bx - vx*Bz
-    du[6] = vx*By - vy*Bx
+    du[4] = vy * Bz - vz * By
+    du[5] = vz * Bx - vx * Bz
+    du[6] = vx * By - vy * Bx
 end
 
 """
@@ -29,14 +38,10 @@ function trace_relativistic_normalized_B!(du, u, p, t)
     vx, vy, vz = @view u[4:6]
     Bx, By, Bz = B(u, t)
 
-    u2 = (vx^2 + vy^2 + vz^2) * v0^2
-    if u2 ≥ c2
-        throw(DomainError(u2, FTLError))
-    end
-    γInv = √(1.0 - u2/c2)
- 
+    γInv = gamma_reciprocal(vx, vy, vz; v0=v0)
+
     du[1], du[2], du[3] = vx, vy, vz
-    du[4] = q2m * γInv *(vy*Bz - vz*By)
-    du[5] = q2m * γInv *(vz*Bx - vx*Bz)
-    du[6] = q2m * γInv *(vx*By - vy*Bx)
+    du[4] = q2m * γInv * (vy * Bz - vz * By)
+    du[5] = q2m * γInv * (vz * Bx - vx * Bz)
+    du[6] = q2m * γInv * (vx * By - vy * Bx)
 end
