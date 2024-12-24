4672e632e02209e9f39090d04b50a5f02e5d4bb6
Beforerr <zzj956959688@gmail.com>
2024-12-23 19:09:01 -0800

    feat: refactor RD_B_field to use RotationDiscontinuity struct for configuration

--- CurrentSheetTestParticle/src/field.jl
+++ CurrentSheetTestParticle/src/field.jl
@@ -2,13 +2,13 @@ const DEFAULT_θ = 45
 const DEFAULT_β = 90
 const DEFAULT_SIGN = 1
 
+
 """
-    RD_B_field([r]; ...)
+    RotationDiscontinuity
 
 Rotating magnetic field. 
 
-# Keyword Arguments
-- `dir` : direction where the field depends on, 1, 2, or 3
+# Arguments
 - `θ` : azimuthal angle, angle between Bn and B0
 - `β` : half of rotation angle
 - `sign` : sign of rotation, 1 for left-handed, -1 for right-handed
@@ -16,7 +16,19 @@ Rotating magnetic field.
 # Notes
 φ = β * tanh(z) is the polar angle
 """
-function RD_B_field(r; dir=3, B=1, θ=DEFAULT_θ, β=DEFAULT_β, sign=DEFAULT_SIGN)
+@kwdef struct RotationDiscontinuity{T<:Number}
+    B::T = 1
+    θ::T = DEFAULT_θ
+    β::T = DEFAULT_β
+    sign::Integer = DEFAULT_SIGN
+end
+
+"""
+# Keyword Arguments
+- `dir` : direction where the field depends on, 1, 2, or 3
+"""
+function B(r, conf::RotationDiscontinuity; dir=3)
+    @unpack θ, β, sign, B = conf
     z = r[dir]
     φ = β * tanh(z)
     Bz = B * cosd(θ)
@@ -25,9 +37,14 @@ function RD_B_field(r; dir=3, B=1, θ=DEFAULT_θ, β=DEFAULT_β, sign=DEFAULT_SI
     return SVector(Bx, By, Bz)
 end
 
+function RD_B_field(r; dir=3, kwargs...)
+    conf = RotationDiscontinuity(; kwargs...)
+    return B(r, conf; dir)
+end
+
 RD_B_field(; kwargs...) = r -> RD_B_field(r; kwargs...)
 
-function TD_B_field(r; dir=3, B=1, By = 0, θ=DEFAULT_θ, β=DEFAULT_β, kw...)
+function TD_B_field(r; dir=3, B=1, By=0, θ=DEFAULT_θ, β=DEFAULT_β, kw...)
     z = r[dir]
     φ = β * tanh(z)
     Bz = B * cosd(θ)
